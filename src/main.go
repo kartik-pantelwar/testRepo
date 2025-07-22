6667f035e2c41a58b7ccddf81c884bfad7f64f62
Kartik Arora <Kartik Arora>
2025-07-22 11:00:31 +0530

    forced push

--- src/main.go
+++ src/main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("First commit")
-	fmt.Println("File changed")
-	fmt.Println("replaced")
-
-	fmt.Println("hello from rashi")
+	fmt.Println("Everything gone")
 }
